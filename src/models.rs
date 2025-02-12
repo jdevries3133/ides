9b3a0881803af7bf06d0d4eb96c70635818891f8
Jack DeVries <jdevries3133@gmail.com>
2025-02-11 20:50:22 -0500

    fix: remove even more auth

--- src/models.rs
+++ src/models.rs
@@ -9,11 +9,3 @@ use sqlx::PgPool;
 pub struct AppState {
     pub db: PgPool,
 }
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct User {
-    pub id: i32,
-    pub username: String,
-    pub email: String,
-    pub created_at: DateTime<Utc>,
-}
